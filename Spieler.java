1fa104456b04b0e72c6c07c741d9af589723ae6c
sebastian.sa <sewaumcka@gmail.com>
2024-05-16 12:53:08 +0200

    SPieler Atribute

--- Spieler.java
+++ Spieler.java
@@ -1,11 +1,16 @@
 public class Spieler {
     int spielerLeben;
     boolean spielerCuffed;
+    String Cuffs = "Du legst dem Dealer Handschellen um du bist 2 mal dran.";
+    int spielerCuffes;
+
     boolean spielerDran;
-    String keineZigaretten = "Du hast keine Zigaretten";
+    String keinItem = "Du hast das Item nicht";
     String zigaretten = "Du rauchst eine Zigarette und regenerierst ein Leben";
     int spielerZigaretten;
+    String saegen = "Du sägst den Lauf der Flinte ab.Jetzt macht sie 2 Damage";
     int spielersaegen;
+    String luppen= "Du guckst in die Flinte die nächste Patrone ist: ";
     int spielerLupen;
     int spielerInventar[] = new int[8];
 
@@ -14,14 +19,27 @@ public class Spieler {
             spielerLeben = spielerLeben +1;
             System.out.println(zigaretten);
         }
-    System.out.println(keineZigaretten);
+    System.out.println(keinItem);
     }
     public void traderHandschellenumlegen(){
-        if(spielerZigaretten >0){
-            int a = 1;
+        if(spielerCuffes >0){
+            System.out.println(Cuffs);
         }
+    System.out.println(keinItem);
 
-
+    }
+    public void lupenutzen() {
+        if (spielerLupen > 0) {
+            System.out.println(luppen);
+            //Array munition nächste munition
+        }
+        System.out.println(keinItem);
+    }
+    public void laufAbsaegen(){
+        if(spielersaegen>0){
+            System.out.println(saegen);
+        }
+    System.out.println(keinItem);
     }
 
 
